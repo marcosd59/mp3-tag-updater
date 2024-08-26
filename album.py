f71b916ad18d805aadd3cde263286f08cdefe918
marcosd59 <damian.marcospool@gmail.com>
2024-08-26 07:37:42 -0500

    Update album metadata for MP3 and FLAC files

--- album.py
+++ album.py
@@ -1,21 +1,33 @@
-# Cambia el nombre del album de todas las canciones.
+#####################################################################################
+#     File Name           :     album.py
+#     Created By          :     Marcos Damian Pool Canul
+#     Creation Date       :     [2024-01-12 10:10]
+#     Last Modified       :     [2024-08-25 19:50]
+#     Description         :     Actualiza el metadato del álbum en archivos MP3 y FLAC
+#                               en la carpeta especificada.
+#####################################################################################
 
 import os
-from mutagen import File
 from mutagen.easyid3 import EasyID3
 from mutagen.flac import FLAC
+
 
 def renombrar_album(ruta, album):
     """
     Actualiza el metadato del álbum en archivos MP3 y FLAC en la carpeta especificada.
+
+    :param ruta: Ruta de la carpeta que contiene los archivos de música.
+    :param album: Nombre del álbum que se asignará a los archivos.
     """
     archivos = os.listdir(ruta)
-    archivos_musica = [archivo for archivo in archivos if archivo.lower().endswith((".mp3", ".flac"))]
+    archivos_musica = [
+        archivo for archivo in archivos if archivo.lower().endswith((".mp3", ".flac"))
+    ]
 
     for archivo in archivos_musica:
         ruta_antigua = os.path.join(ruta, archivo)
 
-        # Actualiza el metadato del álbum
+        # Identifica el tipo de archivo y carga el archivo de audio correspondiente
         if archivo.lower().endswith(".mp3"):
             audio = EasyID3(ruta_antigua)
         elif archivo.lower().endswith(".flac"):
@@ -24,16 +36,18 @@ def renombrar_album(ruta, album):
             print(f"Formato no compatible para -> {archivo}")
             continue
 
+        # Actualiza el metadato del álbum
         audio['album'] = album
         audio.save()
 
-        print(f"Album actualizado a \"{album}\" para -> {archivo}")
+        print(f"Álbum actualizado a \"{album}\" para -> {archivo}")
 
-# ruta = r"D:\LOVE LIVE SCHOOL IDOL PROJECT\Solo Live! collection Memorial BOX III\LoveLive! Solo Live! III from μ’s Nozomi Tojo Memories with Nozomi"
-# album = "Solo Live! collection III Memories with Nozomi"
 
-ruta = input("Ingrese la ruta de la carpeta con los archivos: ")
-album = input("Ingrese el nombre del álbum: ")
+# Ruta de la carpeta con las canciones
+ruta = r"D:\LOVE LIVE SCHOOL IDOL PROJECT\Solo Live! collection Memorial BOX II\Love Live! Solo Live! collection Wakakusa no Season (Hanayo Koizumi)"
+
+# Nuevo álbum para las canciones
+album = "Solo Live! collection II Wakakusa no Season - Koizumi Hanayo"
 
 print("")
 renombrar_album(ruta, album)
